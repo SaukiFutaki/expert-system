7dabfcfa0b42bb7d419e523b1b8e2321a632ca36
SaukiFutaki <utaki.sauki19@gmail.com>
2024-08-24 12:34:40 +0700

    test logic redirect

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-import { createClient } from "./lib/supabase/server";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
