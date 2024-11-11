c525edcde7c76aa838c39015efecbaa17ae11506
SaukiFutaki <utaki.sauki19@gmail.com>
2024-11-11 11:13:48 +0700

    p

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
 import FlowChart from './_components/flowchart'
+import Marquee from '@/components/magicui/maruqee'
 
 export default function Page() {
   return (
-    <div className='flex items-center justify-center flex-col pt-2'>
-        <h1 className='text-4xl'>
-           wlel wlwlwlk lkkak
+    <div className='flex items-center justify-center flex-col pt-2 h-screen'>
+       
+        {/* <Marquee>
+          p by 1 pes
+        </Marquee> */}
+
+        <h1 className='text-7xl'>
+          By x1 pes 
         </h1>
         {/* <Image src="/erd.png" alt="hero" width={1080} height={1080} /> */}
      
